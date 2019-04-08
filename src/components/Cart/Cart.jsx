8763bd26a8ee1e6219b38de45d2b0b66bd3b8869
Kyle Anderson <kyleanderson@Kyles-MacBook-Pro-2.local>
2019-04-08 17:18:50 -0500

    Added functionality that renders empty cart title if cart is empty

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import Title from '../Title/Title';
+import CartColumns from '../Cart/CartColumns';
+import EmptyCart from '../Cart/EmptyCart';
+import {ProductConsumer} from '../Context/Context';
 
 class Cart extends Component {
     render() {
         return (
-            <div>
-                <h3>Hello from cart!</h3>
-            </div>
+            <section>
+                <ProductConsumer>
+                    {value => {
+                        const {cart} = value;
+                        if(cart.length >0) {
+                            return(
+                            <React.Fragment>
+                                <Title name="your" title="cart" />
+                                <CartColumns />
+                            </React.Fragment>
+                            )    
+                        }
+                    }}
+                </ProductConsumer>
+               
+                <EmptyCart />
+            </section>
         );
     }
 }
